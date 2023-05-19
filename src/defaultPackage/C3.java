26d66b06224215bb06a9a887fa6b5bf8776cfc63
TharaThanika05 <133944178+TharaThanika05@users.noreply.github.com>
2023-05-19 09:44:34 +0530

    Modified c3

---
+++ src/defaultPackage/C3.java
@@ -0,0 +1,10 @@
+package defaultPackage;
+
+public class C3 {
+public static void main(String args[])
+{
+	System.out.println("From C3-New Data");
+	System.out.println("New code -> Fresh code");
+}
+
+}
